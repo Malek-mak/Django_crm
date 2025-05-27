25ef0a41241367bf4994b3e692c90f27457784ce
malek <boulahdourabdelmalek@gmail.com>
2025-05-27 19:58:23 +0100

    building the first stage of the crm

--- CRM/website/views.py
+++ CRM/website/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import request, HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .Forms import SignUpForm
 
 # Create your views here.
+def index(request):
+    #logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #cheking if user is real
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in')
+            return redirect('index')
+        else:
+            messages.success(request, 'There was an error, try again.')
+            return redirect('index')
+    else: 
+        return render(request, 'index.html')
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('index')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            # log them in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            messages.success(request, 'You have succefully registerd')
+            return redirect('index')
+            
+            
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form':form})
