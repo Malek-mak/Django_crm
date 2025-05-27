f9015c2b5cae2e38e257e4e5ae4465a8bf1ccf90
malek <boulahdourabdelmalek@gmail.com>
2025-05-27 23:58:38 +0100

    finished the project

--- CRM/website/views.py
+++ CRM/website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import request, HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from .Forms import SignUpForm
+from .Forms import SignUpForm, addRecord
+from .models import record
 
 # Create your views here.
 def index(request):
+    records = record.objects.all
     #logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +22,7 @@ def index(request):
             messages.success(request, 'There was an error, try again.')
             return redirect('index')
     else: 
-        return render(request, 'index.html')
+        return render(request, 'index.html', {'records': records})
 
 
 
@@ -47,3 +49,55 @@ def register(request):
         return render(request, 'register.html', {'form': form})
     
     return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated :
+        customer_record = record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else :
+        messages.success(request, 'You must be logged in')
+        return redirect('index')
+            
+            
+def delete_record(request, pk):
+    if request.user.is_authenticated :
+        delete_record = record.objects.get(id=pk)
+        delete_record.delete()
+        messages.success(request, 'Customer deleted succusfully')
+        return redirect('index') 
+    else: 
+        messages.success(request, 'You must be logged in')
+        return redirect('index')
+    
+    
+def add_record(request):
+    if request.user.is_authenticated :
+        if request.method == 'POST':
+            customer = addRecord(request.POST)
+            if customer.is_valid() :
+                customer.save()
+                messages.success(request, 'Recored added')
+                return redirect('index')
+        else :
+
+            customer = addRecord()
+            return render(request, 'add_record.html', {'customer': customer})
+    else:
+        return redirect('index')
+    
+    return render(request, 'add_record.html', {'customer': customer})
+        
+        
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = record.objects.get(id = pk)
+        form = addRecord(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            return render(request, 'update_record.html', {'form': form})
+        
+    else:
+        return redirect('index')
